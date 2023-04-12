1723cc0f51b5338066f662426045a2595c10cbbc
Michał Miłek <michal.mileq@gmail.com>
2023-04-12 19:26:11 +0200

    Add a test module

---
+++ src/tests.rs
@@ -0,0 +1,15 @@
+#[cfg(test)]
+mod tests {
+
+    extern crate glfw;
+    use crate::cg::model::Model;
+    use cgmath::{vec3, EuclideanSpace};
+
+    #[test]
+    fn model_position() {
+        let mut model = Model::default();
+        let translation = vec3(0.5, 1.0, 5.0);
+        model.translate(translation);
+        assert_eq!(model.position().to_vec(), translation);
+    }
+}
