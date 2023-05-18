a24e2fd8d4b38ff433c31deadbfe2b6561f56efa
sebnow91 <sebnow91@gmail.com>
2023-05-18 16:02:31 +0200

    Add enemy bezier curve

--- src/game/enemy.rs
+++ src/game/enemy.rs
@@ -3,9 +3,9 @@ use std::ops::Sub;
 use super::flight::aircraft::{self, Aircraft, AircraftKind};
 use super::flight::steerable::Steerable;
 use super::missile::EnemyID;
-use crate::cg::consts::{VEC_RIGHT};
+use crate::cg::consts::VEC_RIGHT;
 use crate::{gen_ref_getters, DELTA_TIME};
-use cgmath::{Point3, Quaternion, Vector3, EuclideanSpace, InnerSpace};
+use cgmath::{EuclideanSpace, InnerSpace, Point3, Quaternion, Vector3};
 use image::pnm::ArbitraryHeader;
 use rand::{thread_rng, Rng};
 use vek::{QuadraticBezier3, Vec3};
@@ -25,16 +25,17 @@ impl Enemy {
     /// Create a new enemy with the given aircraft kind
     pub fn new(id: EnemyID, kind: AircraftKind) -> Self {
         let aircraft = Aircraft::new(kind);
-	let rand1 = thread_rng().gen_range(-30., 30.);
-        let rand2 = thread_rng().gen_range(20., 40.);
-        let rand3 = thread_rng().gen_range(-30., 30.);
+        let random_mid = thread_rng().gen_range(10., 20.);
+        let random_length = thread_rng().gen_range(30., 40.);
         let mid = {
             // Select a point in front of the launching aircraft to simulate the missile accelerating
-            let mid = aircraft.model().position() + aircraft.model().front() * 50.;
+            let mid = aircraft.model().position() + aircraft.model().front() * random_mid;
             Vec3::from([mid.x, mid.y, mid.z])
         };
-        let start_point = Vec3::new(0., 0., 0.);
-        let end_point = Vec3::new(rand1, rand2, rand3);
+        let start_point = aircraft.model().position_vek();
+        let end_point = cgmath_to_vek(
+            &(aircraft.model().position() + aircraft.model().front() * random_length).to_vec(),
+        ) + Vec3::new(random_length, 0., random_length);
         let points = Vec3::from([start_point, mid, end_point]);
 
         Self {
@@ -44,7 +45,7 @@ impl Enemy {
             start_point,
             end_point,
             bezier: QuadraticBezier3::from(points),
-	    progress: 0.,
+            progress: 0.,
         }
     }
     pub fn destroy(&mut self) {
@@ -61,21 +62,41 @@ impl Enemy {
     }
     pub fn fly(&mut self) {
         // Progress along the curve
-	if should_change_route(self.aircraft().model().position_vek(), self.end_point) {
-	    self.progress = 0.;
-	    self.start_point = self.aircraft().model().position_vek();
-	    let rand1 = thread_rng().gen_range(-30., 30.);
-            let rand2 = thread_rng().gen_range(20., 40.);
-            let rand3 = thread_rng().gen_range(-30., 30.);
+        if should_continue(self.aircraft().model().position_vek(), self.end_point) {
+            self.progress = 0.;
+            self.start_point = self.aircraft().model().position_vek();
+            let random_mid = thread_rng().gen_range(30., 40.);
+            let random_length = thread_rng().gen_range(60., 80.);
             let mid = {
-		// Select a point in front of the launching aircraft to simulate the missile accelerating
-		let mid = self.aircraft.model().position() + self.aircraft.model().front() * 20.;
-		Vec3::from([mid.x, mid.y, mid.z])
+                // Select a point in front of the launching aircraft to simulate the missile accelerating
+                let mid =
+                    self.aircraft.model().position() + self.aircraft.model().front() * random_mid;
+                Vec3::from([mid.x, mid.y, mid.z])
             };
-	    self.end_point = Vec3::new(rand1, rand2, rand3);
-	    let points = Vec3::from([self.start_point, mid, self.end_point]);
-	    self.bezier = QuadraticBezier3::from(points);
-	}
+            self.end_point = cgmath_to_vek(
+                &(self.aircraft.model().position() + self.aircraft.model().front() * random_length)
+                    .to_vec(),
+            ) + Vec3::new(random_mid, 0., random_mid);
+            let points = Vec3::from([self.start_point, mid, self.end_point]);
+            self.bezier = QuadraticBezier3::from(points);
+        } else if should_return(self.aircraft().model().position_vek()) {
+            self.progress = 0.;
+            self.start_point = self.aircraft().model().position_vek();
+            let random_mid = thread_rng().gen_range(30., 40.);
+            let random_x = thread_rng().gen_range(-40., 40.);
+            let random_y = thread_rng().gen_range(10., 20.);
+            let random_z = thread_rng().gen_range(-40., 40.);
+
+            let mid = {
+                // Select a point in front of the launching aircraft to simulate the missile accelerating
+                let mid =
+                    self.aircraft.model().position() + self.aircraft.model().front() * random_mid;
+                Vec3::from([mid.x, mid.y, mid.z])
+            };
+            self.end_point = Vec3::new(random_x, random_y, random_z);
+            let points = Vec3::from([self.start_point, mid, self.end_point]);
+            self.bezier = QuadraticBezier3::from(points);
+        };
         let t = {
             let bezier = self.bezier;
             let t = 0.001;
@@ -93,24 +114,45 @@ impl Enemy {
 
         self.aircraft_mut().model_mut().set_translation(new_point);
 
-        let vec_to_point = vek_to_cgmath(&self.end_point).sub(self.position().to_vec()).normalize();
+        let vec_to_point = vek_to_cgmath(&self.end_point)
+            .sub(self.position().to_vec())
+            .normalize();
+
         let quat = Quaternion::from_arc(*VEC_RIGHT, vec_to_point, None);
         self.aircraft_mut().model_mut().set_orientation(quat);
-	self.aircraft_mut().model_mut().yaw(-90.);
+        self.aircraft_mut().model_mut().yaw(-90.);
     }
 }
 
-fn should_change_route(start: Vec3<f32>, end: Vec3<f32>) -> bool{
+fn should_return(position: Vec3<f32>) -> bool {
+    if position[0].abs() > 100. || position[1].abs() > 100. || position[2].abs() > 100. {
+        return true;
+    }
+    false
+}
+
+fn should_continue(start: Vec3<f32>, end: Vec3<f32>) -> bool {
     let difference = end - start;
-    dbg!(&difference);
-    if difference[0] < 0.2 && difference[1] < 0.2 && difference[2] < 0.2 {
-	return true;
+    if difference[0].abs() < 0.2 && difference[1].abs() < 0.2 && difference[2].abs() < 0.2 {
+        return true;
     }
     false
 }
 
 fn vek_to_cgmath(v: &Vec3<f32>) -> Vector3<f32> {
-    Vector3 { x: v.x, y: v.y, z: v.z }
+    Vector3 {
+        x: v.x,
+        y: v.y,
+        z: v.z,
+    }
+}
+
+fn cgmath_to_vek(v: &Vector3<f32>) -> Vec3<f32> {
+    Vec3 {
+        x: v.x,
+        y: v.y,
+        z: v.z,
+    }
 }
 
 gen_ref_getters! {
